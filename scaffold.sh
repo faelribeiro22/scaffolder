f22e68b31578a7f91d9c248931ceb51a4c13f19f
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-02-12 16:48:03 -0300

    clone try another way 2

--- scaffold.sh
+++ scaffold.sh
@@ -3,17 +3,12 @@ version="1.0"
 #validate that the user passed the parameter
 npm install --save-dev husky @testing-library/react @testing-library/dom @types/react @types/react-dom jest plop
 npm install --save-dev --save-exact prettier
-cp -a .prettierrc.js .prettierrc.js
-cp -a .prettierignore .prettierignore
-cp -a eslint.config.mjs eslint.config.mjs
-cp -a jest.config.js jest.config.js
 
-jq '.scripts[test:ci] = "jest --runInBand"' package.json > package.json
-jq '.scripts.generate="npx plop --plopfile generators/plopfile.js"' package.json | jq . | > package.json
-jq '.scripts.prepare="husky"' package.json | jq . | > package.json
-jq '.scripts.test:watch="jest --watch --maxWorkers=25%"' package.json | jq . | > package.json
-echo "Generate directories"
+jq '.scripts["test:ci"] = "jest --runInBand"' package.json > package.json
+jq '.scripts["generate"]="npx plop --plopfile generators/plopfile.js"' package.json > package.json
+jq '.scripts["prepare"]="husky"' package.json | jq . | > package.json
+jq '.scripts["test:watch"]="jest --watch --maxWorkers=25%"' package.json > package.json
 
-mkdir -p generators
+# git clone --depth 1 git@github.com:faelribeiro22/scaffolder-files.git
 
-cp -a /generators /generators
+git archive --remote=<git@github.com:faelribeiro22/scaffolder-files.git> HEAD:path/to/directory/ filename | tar -t
