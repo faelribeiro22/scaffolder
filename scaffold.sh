8004f4087c16d41b05ffe98b1bf60cabe1fa3b60
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-02-12 15:32:56 -0300

    change way with get folder name

--- scaffold.sh
+++ scaffold.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 version="1.0"
 #validate that the user passed the parameter
-if [ -z "$1" ]
-then
-     echo "Please add the parameter to script ex: ./scaffold.sh blog"
-    exit
-fi
-#get the project name parameter
-projectName=$1
-#set a list of directories to create
-listOfDirectories="pages components store"
 npm install --save-dev husky @testing-library/react @testing-library/dom @types/react @types/react-dom jest plop
 npm install --save-dev --save-exact prettier
-cp -a .prettierrc.js $projectName
-cp -a .prettierignore $projectName
-cp -a eslint.config.mjs $projectName
-cp -a jest.config.js $projectName
+cp -a .prettierrc.js dirname
+cp -a .prettierignore dirname
+cp -a eslint.config.mjs dirname
+cp -a jest.config.js dirname
 
-cd $projectName
 jq '.scripts.test:ci="jest --runInBand"' package.json | jq . | > package.json
 jq '.scripts.generate="npx plop --plopfile generators/plopfile.js"' package.json | jq . | > package.json
 jq '.scripts.prepare="husky"' package.json | jq . | > package.json
 jq '.scripts.test:watch="jest --watch --maxWorkers=25%"' package.json | jq . | > package.json
 echo "Generate directories"
 
-cp -a /generators $projectName
+cp -a /generators dirname
+
+https://raw.githubusercontent.com/faelribeiro22/scaffolder/refs/heads/main/scaffold.sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/faelribeiro22/scaffolder/refs/heads/main/scaffold.sh)"
