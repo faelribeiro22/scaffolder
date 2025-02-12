49bedac6bca2fe8b3a453ec960777a0ff3541027
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-02-12 16:33:40 -0300

    clone scaffolder repository

--- scaffold.sh
+++ scaffold.sh
@@ -3,19 +3,10 @@ version="1.0"
 #validate that the user passed the parameter
 npm install --save-dev husky @testing-library/react @testing-library/dom @types/react @types/react-dom jest plop
 npm install --save-dev --save-exact prettier
-cp -a .prettierrc.js dirname
-cp -a .prettierignore dirname
-cp -a eslint.config.mjs dirname
-cp -a jest.config.js dirname
 
-jq '.scripts.test:ci="jest --runInBand"' package.json | jq . | > package.json
-jq '.scripts.generate="npx plop --plopfile generators/plopfile.js"' package.json | jq . | > package.json
-jq '.scripts.prepare="husky"' package.json | jq . | > package.json
-jq '.scripts.test:watch="jest --watch --maxWorkers=25%"' package.json | jq . | > package.json
-echo "Generate directories"
+jq '.scripts["test:ci"] = "jest --runInBand"' package.json > package.json
+jq '.scripts["generate"]="npx plop --plopfile generators/plopfile.js"' package.json > package.json
+jq '.scripts["prepare"]="husky"' package.json | jq . | > package.json
+jq '.scripts["test:watch"]="jest --watch --maxWorkers=25%"' package.json > package.json
 
-cp -a /generators dirname
-
-https://raw.githubusercontent.com/faelribeiro22/scaffolder/refs/heads/main/scaffold.sh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/faelribeiro22/scaffolder/refs/heads/main/scaffold.sh)"
+git clone --depth 1 git@github.com:faelribeiro22/scaffolder-files.git
