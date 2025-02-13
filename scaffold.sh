19751f2238b0e83a5b27804c38819e64e7684d49
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-02-13 15:40:24 -0300

    add download jq 2

--- scaffold.sh
+++ scaffold.sh
@@ -4,10 +4,10 @@ sudo apt-get install jq
 npm install --save-dev husky @testing-library/react @testing-library/dom @types/react @types/react-dom jest plop
 npm install --save-dev --save-exact prettier
 
-jq '.scripts["test:ci"] = "jest --runInBand"' package.json > package.json
-jq '.scripts["generate"]="npx plop --plopfile generators/plopfile.js"' package.json > package.json
-jq '.scripts["prepare"]="husky"' package.json | jq . | > package.json
-jq '.scripts["test:watch"]="jest --watch --maxWorkers=25%"' package.json > package.json
+echo $(jq '.scripts["test:ci"] = "jest --runInBand"' package.json) | jq . | > package.json
+echo $(jq '.scripts["generate"]="npx plop --plopfile generators/plopfile.js"') | jq . | package.json > package.json
+echo $(jq '.scripts["prepare"]="husky"' package.json) | jq . | > package.json
+echo $(jq '.scripts["test:watch"]="jest --watch --maxWorkers=25%"') | jq . | > package.json
 
 # git clone --depth 1 git@github.com:faelribeiro22/scaffolder-files.git
 
